9a22a6039e76702e5c91e60b2c1424c4209ea4aa
BruceGuan <41408276+swguan325@users.noreply.github.com>
2021-11-20 18:47:26 +0800

    modify. change interceptor content

---
+++ src/main/java/com/swguan/interceptor/GlobalInterceptor.java
@@ -0,0 +1,33 @@
+package com.swguan.interceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.core.NamedThreadLocal;
+import org.springframework.web.servlet.HandlerInterceptor;
+
+public class GlobalInterceptor implements HandlerInterceptor {
+
+	private NamedThreadLocal<Long> localTimer = new NamedThreadLocal<>("StopWatch-Timer");
+	private Logger logger = LoggerFactory.getLogger(GlobalInterceptor.class);
+
+	@Override
+	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
+			throws Exception {
+		long beginTime = System.currentTimeMillis();
+		localTimer.set(beginTime);
+		return true;
+	}
+
+	@Override
+	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
+			throws Exception {
+		long endTime = System.currentTimeMillis();
+		long beginTime = localTimer.get();
+		long consumeTime = endTime - beginTime;
+		logger.info(String.format("%s consume %d millis", request.getRequestURI(), consumeTime));
+	}
+
+}
--- src/main/java/com/swguan/interceptor/LoginInterceptor.java
+++ src/main/java/com/swguan/interceptor/LoginInterceptor.java
@@ -3,46 +3,29 @@ package com.swguan.interceptor;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.servlet.HandlerInterceptor;
 import org.springframework.web.servlet.ModelAndView;
 
 public class LoginInterceptor implements HandlerInterceptor {
 
+	private Logger logger = LoggerFactory.getLogger(LoginInterceptor.class);
+
 	@Override
 	public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
 			throws Exception {
 		long startTime = System.currentTimeMillis();
-		System.out.println(" --- LoginInterceptor.preHandle Start --- ");
-		System.out.println("Request URL: " + request.getRequestURL());
-		System.out.println("Start Time: " + System.currentTimeMillis());
-
 		request.setAttribute("startTime", startTime);
-		System.out.println(" --- LoginInterceptor.preHandle End --- ");
-
 		return true;
 	}
 
 	@Override
 	public void postHandle(HttpServletRequest request, HttpServletResponse response, Object handler,
 			ModelAndView modelAndView) throws Exception {
-		System.out.println(" --- LoginInterceptor.postHandle Start --- ");
-		System.out.println("Request URL: " + request.getRequestURL());
-		System.out.println(" --- LoginInterceptor.postHandle End --- ");
-	}
-
-	@Override
-	public void afterCompletion(HttpServletRequest request, HttpServletResponse response, Object handler, Exception ex)
-			throws Exception {
-		System.out.println(" ---- LoginInterceptor.afterCompletion Start --- ");
-
 		long startTime = (Long) request.getAttribute("startTime");
 		long endTime = System.currentTimeMillis();
-		System.out.println("Request URL: " + request.getRequestURL());
-		System.out.println("End Time: " + endTime);
-
-		System.out.println("Time Taken: " + (endTime - startTime));
-
-		System.out.println(" ---- LoginInterceptor.afterCompletion End --- ");
+		logger.info(String.format("%s consume %d millis", request.getRequestURI(), (endTime - startTime)));
 	}
 
 }
